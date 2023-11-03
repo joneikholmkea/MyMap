ca4001828ae79ba4aeaf74dca7d1bf2ce6e2cb3e
Jon Eikholm <joneikholm@hotmail.com>
2023-11-03 19:59:37 +0100

    simple save

--- App.js
+++ App.js
@@ -1,7 +1,9 @@
 import { StatusBar } from 'expo-status-bar';
-import { useState } from 'react';
+import { useState, useRef, useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import MapView, {Marker} from 'react-native-maps';
+import * as Location from 'expo-location'
+
 
 export default function App() {
   const [markers, setMarkers]= useState([])
@@ -11,6 +13,40 @@ export default function App() {
     latitudeDelta:20,
     longitudeDelta:20
   })
+
+  const mapView = useRef(null) // ref. til map view objektet.
+  const locationSubscription = useRef(null) // når vi lukker appen, skal den ikke lytte mere
+
+  useEffect(() =>{
+    async function startListening(){
+      let { status } = await Location.requestForegroundPermissionsAsync()
+      if(status !== 'granted'){
+        alert("ingen adgang til lokation")
+        return
+      }
+      locationSubscription.current = await Location.watchPositionAsync({
+        distanceInterval: 100,
+        accuracy: Location.Accuracy.High
+      }, (lokation) =>{
+        const newRegion = {
+          latitude: lokation.coords.latitude,
+          longitude: lokation.coords.longitude,
+          latitudeDelta:20,
+          longitudeDelta:20
+        }
+        setRegion(newRegion) // flytter kortet til den nye lokation
+        if(mapView.current){
+          mapView.current.animateToRegion(newRegion)
+        }
+      })
+    }
+    startListening()
+    return ()=>{ // for at slukke når appen lukker
+      if(locationSubscription.current){
+        locationSubscription.current.remove()
+      }
+    }
+  },[]) 
 
   function addMarker(data){
       const {latitude, longitude} = data.nativeEvent.coordinate
